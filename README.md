22a75503ece95d5890c5d8192061c012e8ecbeee
Richard Patel <me@terorie.dev>
2021-04-26 20:55:14 +0200

    WIP Progress

--- README.md
+++ README.md
@@ -1,3 +1,105 @@
-# Discord InfluxDB exporter
+<center>
+<h1>Discord InfluxDB exporter</h1>
+
+Export Discord chat metrics to InfluxDB time-series DB
+</center>
 
 ![Grafana Preview](preview.png)
+
+## Building
+
+Requirements: Go 1.16
+
+```shell
+git clone https://github.com/terorie/discord-influx.git
+go build -o ./discord-influx .
+```
+
+## Usage
+
+Reference: `./discord-influx --help`
+
+**Environment variables**
+
+| Key                   | Description                                  |
+| --------------------- | -------------------------------------------- |
+| `INFLUXDB_TOKEN`      | InfluxDB API token                           |
+| `INFLUXDB_TOKEN_FILE` | Path to a file containing InfluxDB API token |
+| `DISCORD_TOKEN`       | Discord API token                            |
+| `DISCORD_TOKEN_FILE`  | Path to a file containing Discord API token  |
+
+**Flags**
+
+| Key                           | Description           |
+| ----------------------------- | --------------------- |
+| `--debug`                     | Enables debug logging |
+| `--influxdb-url https://...`  | URL to InfluxDB API   |
+| `--influxdb-org org_name`     | InfluxDB org name     |
+| `--influxdb-bucket my_bucket` | InfluxDB bucket name  |
+
+### Live mode
+
+Live mode listens for new messages indefinitely and continually stats to InfluxDB.
+
+**Usage**
+
+```shell
+export INFLUXDB_TOKEN=xxx
+export DISCORD_TOKEN=xxx
+./discord-influx live \
+  --influxdb-url http://localhost:8086 \
+  --influxdb-org my_org \
+  --influxdb-bucket my_bucket
+```
+
+### History mode
+
+History mode backfills stats of past messages to InfluxDB.
+It runs once until the export is complete.
+
+**Time frame**
+
+The default time frame spans all messages up to the current system time.
+
+The time frame can be adjusted using flags.
+
+**Flags**
+
+| Key                            | Description                            |
+| ------------------------------ | -------------------------------------- |
+| `--start <message_id>`         | Export after this message ID           |
+| `--start 2006-01-02T15:04:05Z` | Export after this RFC 3339 timestamp   |
+| `--stop <message_id>`          | Export before this message ID          |
+| `--start 2007-01-02T15:04:05Z` | Export before this RFC 3339 timestamp  |
+
+**Usage**
+
+```shell
+export INFLUXDB_TOKEN=xxx
+export DISCORD_TOKEN=xxx
+./discord-influx historic \
+  --influxdb-url http://localhost:8086 \
+  --influxdb-org my_org \
+  --influxdb-bucket my_bucket \
+  --start 2006-01-02T15:04:05Z \
+  --stop 212490023738540032
+```
+
+## Metrics
+
+This exporter requires a bucket with 1ns or 1ms accuracy.
+
+With the following labels:
+
+| Name      | Description               |
+| --------- | ------------------------- |
+| `guild`   | Guild ID                  |
+| `channel` | Channel Name              |
+| `emoji`   | Emoji char or reaction ID |
+
+It exports the following time series:
+
+| Name                        | Field   | Labels             |
+| --------------------------- | ------- | ------------------ |
+| `discord_messages`          | `count` | `guild`, `channel` |
+| `discord_message_reactions` | `count` | `guild`, `emoji`   |
--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/signal"
 	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
-	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"go.uber.org/zap"
@@ -43,94 +40,7 @@ var root = cobra.Command{
 func init() {
 	pflags := root.PersistentFlags()
 	pflags.Bool(flagDebug, false, "Enable debug log")
-	root.AddCommand(&live)
-}
-
-var live = cobra.Command{
-	Use:   "live",
-	Short: "Live export stats",
-	Long:  "Exports live event statistics from the channels the Discord Bot is in.",
-	Args:  cobra.NoArgs,
-	PreRun: func(_ *cobra.Command, _ []string) {
-		initDiscord()
-	},
-	Run: func(c *cobra.Command, args []string) {
-		influxURL, err := c.Flags().GetString(flagInfluxURL)
-		if err != nil {
-			panic(err.Error())
-		}
-		if influxURL == "" {
-			log.Fatal("Missing InfluxDB URL")
-		}
-		apiToken := readInfluxToken()
-		influx := influxdb2.NewClient(influxURL, apiToken)
-		defer influx.Close()
-		influxOrg, err := c.Flags().GetString(flagInfluxOrg)
-		if err != nil {
-			panic(err.Error())
-		}
-		influxBucket, err := c.Flags().GetString(flagInfluxBucket)
-		if err != nil {
-			panic(err.Error())
-		}
-		writeAPI := influx.WriteAPI(influxOrg, influxBucket)
-		defer writeAPI.Flush()
-		defer discord.Close()
-		log.Info("Starting Discord-InfluxDB live exporter")
-		defer log.Info("Stopping")
-		discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-			log := log.With(
-				zap.String("guild_id", m.GuildID),
-				zap.String("channel_id", m.ChannelID),
-				zap.String("message_id", m.Message.ID))
-			t, err := m.Timestamp.Parse()
-			if err != nil {
-				log.Warn("Invalid timestamp on message, ignoring", zap.Error(err))
-				return
-			}
-			writeAPI.WritePoint(write.NewPointWithMeasurement("discord_messages").
-				SetTime(t).
-				AddTag("guild", m.GuildID).
-				AddTag("channel", m.ChannelID).
-				AddField("count", 1))
-			log.Debug("MessageCreate")
-		})
-		discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
-			log := log.With(
-				zap.String("guild_id", m.GuildID),
-				zap.String("channel_id", m.ChannelID),
-				zap.String("message_id", m.MessageID),
-				zap.String("emoji", m.Emoji.Name))
-			writeAPI.WritePoint(write.NewPointWithMeasurement("discord_message_reactions").
-				AddTag("guild", m.GuildID).
-				AddTag("emoji", m.Emoji.Name).
-				AddField("count", 1))
-			log.Debug("MessageReactionAdd")
-		})
-		discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageReactionRemove) {
-			log := log.With(
-				zap.String("guild_id", m.GuildID),
-				zap.String("channel_id", m.ChannelID),
-				zap.String("message_id", m.MessageID),
-				zap.String("emoji", m.Emoji.Name))
-			writeAPI.WritePoint(write.NewPointWithMeasurement("discord_message_reactions").
-				AddTag("guild", m.GuildID).
-				AddTag("emoji", m.Emoji.Name).
-				AddField("count", -1))
-			log.Debug("MessageReactionRemove")
-		})
-		discord.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMessageReactions
-		if err := discord.Open(); err != nil {
-			log.Fatal("Failed to connect to Discord", zap.Error(err))
-		}
-		interrupt := make(chan os.Signal, 1)
-		signal.Notify(interrupt, os.Interrupt, os.Kill)
-		<-interrupt
-	},
-}
-
-func init() {
-	addInfluxFlags(live.Flags())
+	root.AddCommand(&live, &historic)
 }
 
 const (
@@ -221,3 +131,15 @@ func readInfluxToken() (influxToken string) {
 	}
 	return
 }
+
+// InfluxDB identifiers.
+const (
+	metricMessages  = "discord_messages"
+	metricReactions = "discord_message_reactions"
+
+	labelGuild   = "guild"
+	labelChannel = "channel"
+	labelEmoji   = "emoji"
+
+	fieldCount = "count"
+)
